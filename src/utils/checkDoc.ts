60af6f369ac2b8b018e18ea268e8710be03a9f49
Varun0157 <varun.edachali@gmail.com>
2023-12-08 19:46:21 +0530

    Simplified check document

--- src/utils/checkDoc.ts
+++ src/utils/checkDoc.ts
@@ -5,12 +5,5 @@ const BUNDLE_FILE_NAME_ENDINGS = [".zzb"] as const;
 export function documentIsBundle(document: TextDocument): boolean {
   const docFsPath = document.uri.fsPath;
 
-  let docIsBundle: boolean = false;
-  BUNDLE_FILE_NAME_ENDINGS.forEach((ENDING) => {
-    if (docFsPath.endsWith(ENDING)) {
-      docIsBundle = true;
-    }
-  });
-
-  return docIsBundle;
+  return BUNDLE_FILE_NAME_ENDINGS.some((ENDING) => docFsPath.endsWith(ENDING));
 }
