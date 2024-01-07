c280ffe92305fd7ab7cd94aaeca0483be8d51283
Varun0157 <varun.edachali@gmail.com>
2024-01-07 19:41:06 +0530

    feat (file-contents): replacing file contents only if file:// is the only item in the body

--- src/reformatRequest.ts
+++ src/reformatRequest.ts
@@ -1,3 +1,6 @@
+import * as path from "path";
+import * as fs from "fs";
+
 import {
   GotRequest,
   RequestSpec,
@@ -5,17 +8,40 @@ import {
   getCurlRequest,
   replaceVariablesInRequest,
 } from "zzapi";
-import { replaceFileContents } from "./fileContents";
+
+import { getWorkingDir } from "./utils/pathUtils";
+
 import { getVarStore } from "./variables";
 
-function formatAndReplaceVars(request: RequestSpec, extensionVersion: string): string[] {
+function replaceFileContents(body: any, replaceWithFileRef?: boolean): any {
+  if (typeof body !== "string") return body;
+
+  /*
+  finds all file:// instances with atleast 1 succeeding word character
+  matches the file-name referred to by this instance
+  */
+  const fileRegex = /file:\/\/([^\s]+)/g; 
+  return body.replace(fileRegex, (match, givenFilePath) => {
+    if (match !== body) return match; // we only perform a replacement if file:// is the ENTIRE body
+
+    if (replaceWithFileRef === true) return `@${givenFilePath}`;
+    const filePath = path.resolve(getWorkingDir(), givenFilePath);
+    return fs.readFileSync(filePath, "utf-8");
+  });
+}
+
+function formatAndReplaceVars(
+  request: RequestSpec,
+  extensionVersion: string,
+  curlReq?: boolean,
+): string[] {
   const autoHeaders: { [key: string]: string } = {};
   autoHeaders["user-agent"] = "zzAPI-vscode/" + extensionVersion;
   if (request.httpRequest.body && typeof request.httpRequest.body == "object")
     autoHeaders["content-type"] = "application/json";
   request.httpRequest.headers = Object.assign(autoHeaders, request.httpRequest.headers);
 
-  request.httpRequest.body = replaceFileContents(request.httpRequest.body);
+  request.httpRequest.body = replaceFileContents(request.httpRequest.body, curlReq);
   return replaceVariablesInRequest(request, getVarStore().getAllVariables());
 }
 
@@ -33,7 +59,7 @@ export function getCurlOfReq(
   request: RequestSpec,
   extensionVersion: string,
 ): { curlRequest: string; undefinedVars: string[] } {
-  const undefs = formatAndReplaceVars(request, extensionVersion);
+  const undefs = formatAndReplaceVars(request, extensionVersion, true);
   const curlCommand = getCurlRequest(request);
 
   return { curlRequest: curlCommand, undefinedVars: undefs };
