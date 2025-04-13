046f8852cd1d8aa678dbe912711a7050ad06bcae
Prashant Sharma <prashant.sharma@agrostar.in>
2025-04-13 15:32:26 +0530

    added parse/convert curl logic

--- src/runImportCurl.ts
+++ src/runImportCurl.ts
@@ -1,25 +1,25 @@
 import { env, window } from "vscode";
-import { convertCurl } from "zzapi";
 
 import { openDocument } from "./showInEditor";
+import convertCurl from "./convertCurl";
 
 export default async function importCurl(): Promise<void> {
- try {
-   const clipboardText = await env.clipboard.readText()
+  try {
+    const clipboardText = await env.clipboard.readText();
 
-   if (!clipboardText || clipboardText.length < 1) {
-     throw new Error("cURL not copied.");
-   }
-   if (!clipboardText?.toLowerCase()?.trimStart()?.startsWith("curl ")) {
-     throw new Error("Invalid cURL.");
-   }
+    if (!clipboardText || clipboardText.length < 1) {
+      throw new Error("cURL not copied.");
+    }
+    if (!clipboardText?.toLowerCase()?.trimStart()?.startsWith("curl ")) {
+      throw new Error("Invalid cURL.");
+    }
 
-   const content = convertCurl(clipboardText);
-   await openDocument(content, "yaml");
- } catch (e: any) {
-   console.log(e);
-   console.log(e.message);
-   window.showErrorMessage(e.message, { modal: true });
-   return;
- }
+    const content = convertCurl(clipboardText);
+    await openDocument(content, "yaml");
+  } catch (e: any) {
+    console.log(e);
+    console.log(e.message);
+    window.showErrorMessage(e.message, { modal: true });
+    return;
+  }
 }
